9fb326d07d67fe62e69b3812c6b2bc5bcf3026af
Diyan Tonchev <diyantonchev@gmail.com>
2017-03-01 16:38:07 +0200

    updated readme and package.json

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const socketStream = require('socket.io-stream');
 const path = require('path');
 const fs = require('fs');
 const mongoose = require('mongoose');
